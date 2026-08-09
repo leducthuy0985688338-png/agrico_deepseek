dd37ad1e928003afb82131b073aaa10bd699aa10
leducthuy0985688338-png <leducthuy0985688338@gmail.com>
2026-08-09 10:34:08 +0700

    fix: remove duplicate Flutter Gradle plugin declaration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,12 +1,3 @@
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "7.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
-    
-    // 👇 THÊM DÒNG NÀY ĐỂ CẤU HÌNH FIREBASE
-    id("com.google.gms.google-services") version "4.5.0" apply false
-}
-
 allprojects {
     repositories {
         google()
@@ -15,6 +6,7 @@ allprojects {
 }
 
 rootProject.buildDir = File(rootProject.projectDir, "../build")
+
 subprojects {
     project.buildDir = File(rootProject.buildDir, "${project.name}")
 }
