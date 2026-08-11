81b87a3f313e250cdbcdb9d1d6c1f493f75b32ee
leducthuy0985688338-png <leducthuy0985688338@gmail.com>
2026-08-11 07:38:35 +0700

    feat: map finance records for cloud restore

--- lib/models/finance_model.dart
+++ lib/models/finance_model.dart
@@ -23,6 +23,36 @@ class FinanceRecord {
     this.machineId,
     this.machineName,
   });
+
+  factory FinanceRecord.fromMap(Map<String, dynamic> map) {
+    return FinanceRecord(
+      id: map['id'] as String,
+      fieldId: map['fieldId'] as String,
+      fieldName: map['fieldName'] as String,
+      date: DateTime.parse(map['date'] as String),
+      type: TransactionType.values[(map['type'] as num).toInt()],
+      category: map['category'] as String,
+      amount: (map['amount'] as num).toDouble(),
+      description: map['description'] as String?,
+      machineId: map['machineId'] as String?,
+      machineName: map['machineName'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'fieldId': fieldId,
+      'fieldName': fieldName,
+      'date': date.toIso8601String(),
+      'type': type.index,
+      'category': category,
+      'amount': amount,
+      'description': description,
+      'machineId': machineId,
+      'machineName': machineName,
+    };
+  }
 }
 
 enum TransactionType { THU, CHI }
