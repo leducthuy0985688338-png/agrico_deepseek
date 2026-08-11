85a58ce327a4d9c8993d45de343090d9928e1eb9
leducthuy0985688338-png <leducthuy0985688338@gmail.com>
2026-08-11 10:53:10 +0700

    feat: load production costs from cloud

--- lib/services/cloud_service.dart
+++ lib/services/cloud_service.dart
@@ -132,6 +132,24 @@ class CloudService {
     }).toList(growable: false);
   }
 
+  static Future<List<ProductionCostModel>> loadProductionCosts() async {
+    final snapshot = await db
+        .collection('production_costs')
+        .orderBy('date', descending: true)
+        .get();
+
+    return snapshot.docs.map((doc) {
+      final data = Map<String, dynamic>.from(doc.data());
+      final date = data['date'];
+      if (date is Timestamp) {
+        data['date'] = date.toDate().toUtc().toIso8601String();
+      } else if (date is DateTime) {
+        data['date'] = date.toUtc().toIso8601String();
+      }
+      return ProductionCostModel.fromJson(data);
+    }).toList(growable: false);
+  }
+
   static Future<void> saveDistanceMeasurement(DistanceMeasurementModel measurement) async {
     await db.collection('distance_measurements').doc(measurement.id).set({
       'id': measurement.id,
